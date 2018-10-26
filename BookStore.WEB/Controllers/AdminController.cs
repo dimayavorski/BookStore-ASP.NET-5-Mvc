d45bd21a50b454b39b60a960edc50aeead98ee96
dmitriy <dimonslayerrooock@mail.ru>
2018-10-26 19:54:37 +0300

    Created AdminController and switched BookCards functionality

---
+++ BookStore.WEB/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStore.WEB.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
